2e2decb9abbf415947554e4736d98880dc0f4976
gilzoide <gilzoide@gmail.com>
2015-10-04 17:50:01 -0300

    F#/New Script
    NEW: F#/New Script menu item, which pops a file dialog, and writes the
    default namespace/type stuff

---
+++ editorScripts/FSCompilerNewScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+/// New Script utility
+public class FSCompilerNewScript {
+	[MenuItem ("F#/New Script")]
+	public static void NewFSScript () {
+		string path = EditorUtility.SaveFilePanelInProject ("New F# Script", "myFsScript", "fs", "oie");
+		string script = Path.GetFileNameWithoutExtension (path);
+		File.WriteAllText (path, "namespace " + script +
+				"\n\nopen UnityEngine\n\ntype " + script + " () =\n    inherit MonoBehaviour ()\n\n    member this.Start () = ()\n\n    member this.Update () = ()\n");
+	}
+}
